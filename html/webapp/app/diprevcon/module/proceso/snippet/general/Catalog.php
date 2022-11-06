dbe2c29675b3ce654f3088b76499c500ebf2b642
raquel-hash <55359633+raquel-hash@users.noreply.github.com>
2022-11-06 00:18:34 -0400

    module proceso

---
+++ html/webapp/app/diprevcon/module/proceso/snippet/general/Catalog.php
@@ -0,0 +1,17 @@
+<?PHP
+namespace App\Diprevcon\Module\Proceso\Snippet\general;
+use Core\CoreResources;
+
+class Catalog extends CoreResources{
+
+    function __construct(){
+        /**
+         * Inicializamos todas las librerias y variables para el submodulo
+         */
+        $this->appInit();
+    }
+
+    public function confCatalog(){
+    }
+
+}
