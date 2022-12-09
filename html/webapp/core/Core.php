304f4bcf06d44a83ec9914e857ec6e5e0fa220d1
Henry Taby <henry.taby@gmail.com>
2022-12-09 04:51:54 -0400

    core fix

--- html/webapp/core/Core.php
+++ html/webapp/core/Core.php
@@ -76,11 +76,11 @@ class Core{
         }
 
         $res =array();
-        $res["app"] = $app;
-        $res["module"] = $module;
-        $res["core_id"] = isset($core_id)?$core_id:"";
-        $res["route_action"] = $ruta_accion;
-        $res["vars"] = $variables;
+        $res["app"] = $app??"";
+        $res["module"] = $module??"";
+        $res["core_id"] = $core_id ?? "";
+        $res["route_action"] = $ruta_accion??"";
+        $res["vars"] = $variables?? "";
         return $res;
     }
 
